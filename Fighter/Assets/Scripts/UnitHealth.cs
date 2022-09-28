e8f3d7bc2e2a39c4a876a74ff45d8e35f98c52b9
Stephen <gstephenc@hotmail.com>
2022-09-28 18:07:59 -0400

    created UnitHealth

---
+++ Fighter/Assets/Scripts/UnitHealth.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth
+{
+    //fields: values that make up the class
+    int _currentHealth;
+    int _maxHealth;
+    
+    //constructor: used to instantiate the class
+    public UnitHealth(int health, int max)
+    {
+        _maxHealth = max;
+        _currentHealth = health;
+    }
+
+    //properties: allows access to fields to get or set 
+    public int Health
+    {
+        get
+        {
+            return _currentHealth;
+        }
+        set
+        {
+            _currentHealth = value;
+        }
+    }
+
+    public int MaxHealth
+    {
+        get
+        {
+            return _maxHealth;
+        }
+        set
+        {
+            _maxHealth = value;
+        }
+    }
+
+    //methods
+    public void dmgUnit(int dmg)
+    {
+        if(_currentHealth < 0)
+        {
+        _currentHealth -= dmg;
+        }
+    }
+
+
+    public void healUnit(int heal)
+    {
+        if (_currentHealth < _maxHealth)
+        {
+            if(_currentHealth + heal > _maxHealth)
+            {
+                _currentHealth = _maxHealth;
+            }
+            else
+            {
+                _currentHealth += heal;
+            }
+        }
+    }
+
+}
