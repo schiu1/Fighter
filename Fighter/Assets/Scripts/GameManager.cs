21bdf6fb01a01ddc2d356c6f3d9ecbaba36284df
Stephen <gstephenc@hotmail.com>
2022-09-28 18:20:22 -0400

    created GameManager
     -created GameManager object and script holding player1 and 2 health but not linked to their gameobjects yet

---
+++ Fighter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //this is what makes a class a Singleton
+    public static GameManager gameManager { get; private set; }
+
+    //player1 and player2
+    public UnitHealth _p1Health = new UnitHealth(100, 100);
+    public UnitHealth _p2Health = new UnitHealth(100, 100);
+
+    void Awake()
+    {
+        if(gameManager != null && gameManager != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            gameManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
