fa203b7eb288e347e2d564a9edc4bfacc3b58f45
Stephen <gstephenc@hotmail.com>
2022-10-12 16:50:49 -0400

    added parallax effect
    - adjusted camera settings
    - Parallax effect set up for background

---
+++ Fighter/Assets/Scripts/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    float startPos;
+    GameObject camObj;
+    [SerializeField] float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camObj = GameObject.Find("Main Camera");
+        startPos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (camObj.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
