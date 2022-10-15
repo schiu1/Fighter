a2327da9308fcf207a5b2fd89564a5db0fcd30f8
Stephen <gstephenc@hotmail.com>
2022-10-15 19:01:24 -0400

    started round system
    - added scores to Canvas object
    - created Canvas script
    - GameManager now records scores
    - Canvas script displays scores from GameManager on UI when round starts
    - need to find how to display updated score when round ends

---
+++ Fighter/Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour
+{
+    Text p1ScoreUI;
+    Text p2ScoreUI;
+    GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.gameManager;
+        p1ScoreUI = transform.Find("P1_Score").GetComponent<Text>();
+        p2ScoreUI = transform.Find("P2_Score").GetComponent<Text>();
+        p1ScoreUI.text = gm.p1Score.ToString();
+        p2ScoreUI.text = gm.p2Score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Fighter/Assets/Scripts/GameManager.cs
+++ Fighter/Assets/Scripts/GameManager.cs
@@ -11,6 +11,10 @@ public class GameManager : MonoBehaviour
     public UnitHealth _p1Health = new UnitHealth(100, 100);
     public UnitHealth _p2Health = new UnitHealth(100, 100);
 
+    public int p1Score = 0;
+    public int p2Score = 0;
+
+
     void Awake()
     {
         if(gameManager != null && gameManager != this)
@@ -23,4 +27,27 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
     }
+
+    //keep track of healthbar in GameManager and Canvas
+    //if there is winner, increment score in GameManager
+    //Canvas will be checking what changed in GameManager when health becomes 0 and replicate that
+    //Canvas will display either round win banner for 5 seconds or game win banner until user responds
+    //if round win, GameManager will wait 5 sec, then reset scene
+    //if game win, GameManager will wait for user input
+
+    void Update()
+    {
+        if (_p1Health.Health <= 0)
+        {
+            p2Score += 1;
+            //determine if roundwin or gamewin and call endround with that as arguement
+        }
+        else if(_p2Health.Health <= 0)
+        {
+            p2Score += 1;
+        }
+
+    }
+
+    
 }
