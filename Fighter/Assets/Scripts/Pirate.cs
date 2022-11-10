aa72b06e48b7f71e7ff7778e737a4de5ca04d8a5
Stephen <gstephenc@hotmail.com>
2022-11-10 18:34:52 -0500

    WinAnim pirate now moves
    - WinAnim pirate now moves towards the winner
    - only when it collides will it "disappear", allowing for may's spin anim with pirate to begin
    - when collider touches loser, nothing happens
    
    - issue with pirate not ignoring loser's collider and getting stuck on it
    - currently implemented with a capsule collider to keep it grounded, which also prevents movement through walls
    - might need to ignore rightwall as well and extend ground

---
+++ Fighter/Assets/Scripts/Pirate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pirate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //make it on spawn ignore the collision of the loser
+        //determine if the ignored player is p1 or p2 based on health
+        //find the ignored player and set variable to them
+        //call Physics2D.IgnoreCollision on their capsulecollider    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(-1.5f, 0) * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if((GameManager.gameManager._p1Health.Health > GameManager.gameManager._p2Health.Health) 
+            && collision.gameObject.tag == "Player1")
+        {
+            Destroy(gameObject);
+        }
+
+        else if ((GameManager.gameManager._p1Health.Health < GameManager.gameManager._p2Health.Health)
+            && collision.gameObject.tag == "Player2")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
