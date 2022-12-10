f4a478c24020da67ca976c22a604124be5a326ab
Stephen <gstephenc@hotmail.com>
2022-12-10 18:22:28 -0500

    highlight issue
    - fixed first selected button not being highlighted on enable
    - new issue where if the pause menu closes with first selected highlighted, it doesn't highlight next onenable
    - another new issue where mixing the two input types (wsad/arrows) deselects the button

---
+++ Fighter/Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject firstSelected;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(firstSelected);
+    }
+}
