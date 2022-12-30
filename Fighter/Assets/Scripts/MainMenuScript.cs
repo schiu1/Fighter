519893e15da0f9b7f844b1840971988c5bf37854
Stephen <gstephenc@hotmail.com>
2022-12-30 17:58:28 -0500

    main menu highlights and play works
    - both buttons in main menu now highlight
    - play button loads and then switches to fight_scene when pressed
    - odd error saying it failed to set the active scene when it looks like it did

---
+++ Fighter/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    bool fightSceneLoaded;
+    void Awake()
+    {
+        fightSceneLoaded = false;
+    }
+
+    public void StartGame()
+    {
+        LoadFightScene();
+        if (fightSceneLoaded)
+        {
+            Debug.Log(SceneManager.GetSceneByName("Fight_Scene").name);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("Fight_Scene"));
+        }
+    }
+
+    void LoadFightScene()
+    {
+        SceneManager.LoadScene("Fight_Scene");
+        fightSceneLoaded = !fightSceneLoaded;
+    }
+}
