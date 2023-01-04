9baaf00cbc0be28f494fe6a3465c1963b6e246f1
Stephen <gstephenc@hotmail.com>
2023-01-04 16:54:50 -0500

    added background music to main menu
    - made modified version of AudioManager that is not a singleton just for the main menu
    - need to add button in pause menu to return to main menu/make quit button return to main menu

---
+++ Fighter/Assets/Scripts/MainMenuAudio.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAudio : MonoBehaviour
+{
+    [SerializeField]
+    Sound[] sounds = null;
+    //[HideInInspector]
+    public float masterVolume;
+
+    void Awake()
+    {
+        //for each sound in array, 
+        foreach (Sound s in sounds)
+        {
+            //assign the AudioSource variable in Sound an instance of AudioSource
+            //and assign the properties saved in Sound obj to the AudioSource obj
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.name = s.soundName;
+            s.source.volume = s.volume;
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+        }
+        masterVolume = 0.5f;
+    }
+
+    void Start()
+    {
+        PlaySound("Theme");
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound found = null;
+        foreach (Sound s in sounds)
+        {
+            if (s.soundName == name)
+            {
+                found = s;
+            }
+        }
+        if (found == null)
+        {
+            Debug.Log("Sound with " + name + " was not found");
+            return;
+        }
+        found.source.Play();
+    }
+
+    public void ChangeAllVolume(float value)
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source.volume = value;
+        }
+    }
+}
