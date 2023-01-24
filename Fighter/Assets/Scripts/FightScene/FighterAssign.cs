4ee785e35ebe3f3752e480264c157cd771a4b113
Stephen <gstephenc@hotmail.com>
2023-01-24 18:44:18 -0500

    working on fighter assignment in Fight_Scene
    - created FighterAssign to enable the correct fighter selected
    - will be for both players, but won't include player2 for now due to there being no player2 fighter select
    - uses PlayerPref to get name of player, so i question the use of having names in GM now
    - need to make another fighter and maybe rework structure of fighter to create fighters easier
    - maybe add hitstun to blocking

---
+++ Fighter/Assets/Scripts/FightScene/FighterAssign.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterAssign : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] fighters = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(GameObject fighter in fighters)
+        {
+            if(fighter.name == PlayerPrefs.GetString("player1") && gameObject.name == "Player1")
+            {
+                fighter.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
