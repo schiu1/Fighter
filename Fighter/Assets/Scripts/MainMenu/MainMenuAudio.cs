d3f12aa5f695cecb2add62372e70fcab53e175d1
Stephen <gstephenc@hotmail.com>
2023-01-28 18:26:32 -0500

    trying to use PlayerPref to save volume setting
    - issue with fight_scene showing 0.5 when slider is far left when first opened
    - issue when going back to main_menu after fight_scene triggers else statement as if its a first run of the game

--- Fighter/Assets/Scripts/MainMenu/MainMenuAudio.cs
+++ Fighter/Assets/Scripts/MainMenu/MainMenuAudio.cs
@@ -25,7 +25,17 @@ public class MainMenuAudio : MonoBehaviour
 
     void Start()
     {
-        masterVolume = SystemSettings.systemSettings.masterVolume;
+        //masterVolume = SystemSettings.systemSettings.masterVolume;
+        if(PlayerPrefs.GetFloat("masterVolume") != 0)
+        {
+            masterVolume = PlayerPrefs.GetFloat("masterVolume");
+        }
+        else
+        {
+            Debug.Log(PlayerPrefs.GetFloat("masterVolume"));
+            masterVolume = 0.5f;
+        }
+
         foreach (Sound s in sounds)
         {
             s.source.volume = masterVolume;
@@ -57,7 +67,8 @@ public class MainMenuAudio : MonoBehaviour
         {
             s.source.volume = value;
         }
-        SystemSettings.systemSettings.masterVolume = value;
+        //SystemSettings.systemSettings.masterVolume = value;
+        PlayerPrefs.SetFloat("masterVolume", value);
         GameObject.Find("VolumeValue").GetComponent<Text>().text = value.ToString("0.##");
     }
 }
