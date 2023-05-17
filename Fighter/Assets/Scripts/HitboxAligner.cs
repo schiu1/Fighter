9e5eeec87d167d4c4f629b47d9878e7b2edaef5e
Stephen <gstephenc@hotmail.com>
2023-05-17 18:15:53 -0400

    created HitboxAligner script
    - will use this to change hitbox position when fighter is p2
    - undecided if script is in all hitboxes or just fighter gameobject

---
+++ Fighter/Assets/Scripts/HitboxAligner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxAligner : MonoBehaviour
+{
+    //formula for changing hitbox is -[x value] + differential
+    //for bridget it is .18
+    //check if player1 or player2 
+    //make it a method and call method in PlayerAssign
+    [SerializeField]
+    float differential = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
